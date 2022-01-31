42f6ecf98960bf730f23c23755af8c5a35e8d187
Mauricio Silva <mrcvr1@gmail.com>
2022-01-31 11:26:58 -0300

    aula 3

--- src/index.js
+++ src/index.js
@@ -1,19 +1,50 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { PrismaClient } = require('@prisma/client');
+const  prisma = new PrismaClient();
 
 const typeDefs = gql`
- type User {
-   nome: String
- }
- type Query {
-   users: [User]
- }
+  scalar DateTime
+  type User {
+    id: Int
+    nome: String
+    email: String
+    createdAt: DateTime
+    posts: [Post]
+  }
+
+  type Post {
+    id: Int
+    titulo: String
+    conteudo: String
+  }
+
+  type Query {
+    users: [User]
+    postsByUser (id: Int): [Post]
+    postsByReviewer (id: Int): [Post]
+  }
 `
 
 const resolvers = {
- Query: {
-   users: () => [{ nome: "Ana" }, {nome: "Bia" }]
- }
+  Query: {
+    users: async () => await prisma.user.findMany({
+      include: {
+        posts: true
+      }
+    }),
+    postsByUser: async (_, args) => {
+      return prisma.user
+        .findUnique({ where: { id: Number(args.id) } })
+        .posts()
+    },
+    postsByReviewer: async (_, args) => {
+      return prisma.review
+        .findUnique({ where: { id: Number(args.id) } })
+        .reviewer()
+        .posts()
+    }
+  }
 }
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers, context: prisma })
 server.listen({ port: 4000 }, () => console.log(`Servidor pronto em localhost:4000`)) 
